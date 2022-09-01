49ba151a13f4aa8244ac2a77d9732f9cee818221
svmpsy <112634960+svmpsy@users.noreply.github.com>
2022-09-01 18:37:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/svmpsy/psyscaling/",
-    packages=find_packages(),
+    packages=['psyscaling'],
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
