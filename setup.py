c4508b974f36b5726bbd06b793978847b4e37171
svmpsy <112634960+svmpsy@users.noreply.github.com>
2022-09-01 18:00:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,23 @@
-#!/usr/bin/env python
-import sys
-import platform
-from setuptools import setup
-# from Cython.Build import cythonize
+from setuptools import setup, find_packages
 
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
 
-def get_version():
-    with open("version.py", "rt") as f:
-        return f.readline().split("=")[1].strip(' "\n')
-
-
-is_cpython = platform.python_implementation() == 'CPython'
-py_version = sys.version_info[:2]
-
-
-extras_require = {'fast': []}
-if is_cpython:
-    extras_require['fast'].append("DAWG >= 0.8")
-    if py_version < (3, 5):
-        # lru_cache is optimized in Python 3.5
-        extras_require['fast'].append("fastcache >= 1.0.2")
-
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
-    name='psyscaling',
-    version=get_version(),
-    author='Svetlana Morozova',
-    author_email='svmpsy1@gmail.com',
-    url='https://github.com/svmpsy/psyscaling',
-
-    description='Psychological scaling of text and graphic information. Used to prepare experimental data for quantitative analysis. Supports only Russian language now.',
-    long_description=open('README.md').read(),
-
-    license='MIT license',
-    packages=[
-        'psyscaling',
-    ]
+    name="psyscaling",
+    version="0.0.1",
+    author="Svetlana Morozova",
+    author_email="svmpsy1@gmail.com",
+    description="Psychological scaling of text and graphic information. Used to prepare experimental data for quantitative analysis.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/svmpsy/psyscaling/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
 )
